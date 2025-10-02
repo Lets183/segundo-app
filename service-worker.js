cb66a252222c0d18defda877543329a5091b3c88
Letícia Lima <letslimadias183@gmail.com>
2025-10-02 11:03:06 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("loja-roupas-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon-000.jpg",
+        "icone-111.jpg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
